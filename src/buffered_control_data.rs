076244960796ce6fda1f9498f29c94dc45283c5b
Pavel Zakopaylo <zakopaylo2@gmail.com>
2018-09-07 17:47:41 +1000

    Added conversion of control data

--- src/buffered_control_data.rs
+++ src/buffered_control_data.rs
@@ -1,3 +1,5 @@
+use super::ControlData;
+
 #[derive(Copy, Clone, Debug)]
 pub struct BufferedControlData {
     pub rudder: f32,
@@ -16,6 +18,27 @@ impl BufferedControlData {
             right_aileron: 0.0,
             elevator: 0.0,
             throttle: 0.0,
+        }
+    }
+
+    pub fn from_external(cd: ControlData) -> Self {
+        assert!(cd.verify());
+
+        let max_deflection_deg: f32 = 15f32; // relative to zero in either direction
+        // see comments in ControlData struct
+        let control_surface_conversion = |input: u16| -> f32 {
+            (input as f32) * ((2f32 * max_deflection_deg) / ((2 << 11) - 1) as f32)
+                - max_deflection_deg
+        };
+
+        BufferedControlData {
+            rudder: control_surface_conversion(cd.rudder),
+            left_aileron: control_surface_conversion(cd.left_aileron),
+            right_aileron: control_surface_conversion(cd.right_aileron),
+            elevator: control_surface_conversion(cd.elevator),
+
+            // throttle output is just [0, 1] so we divide it by the full range
+            throttle: (cd.throttle as f32) / (((2 << 11) - 1) as f32),
         }
     }
 }
--- src/control_data.rs
+++ src/control_data.rs
@@ -8,13 +8,13 @@ pub struct ControlData {
     sync: [u8; 4],
 
     // 11 bits each; unsigned so at 0 control surface is down.
-    rudder: u16,
-    left_aileron: u16,
-    right_aileron: u16,
-    elevator: u16,
+    pub rudder: u16,
+    pub left_aileron: u16,
+    pub right_aileron: u16,
+    pub elevator: u16,
 
     // also 11 bit unsigned
-    throttle: u16,
+    pub throttle: u16,
     _pad: u16, // better to be explicit
 
     // Sum of bytes between sync and checksum, modulo 4 bytes, all bits flipped (1's complement)
