176045ecf5ce9c7aa25a2e18179b2e78c147e1e6
Pavel Zakopaylo <zakopaylo2@gmail.com>
2018-09-12 21:19:51 +1000

    Added debug info to checksum checking

--- src/control_data.rs
+++ src/control_data.rs
@@ -26,8 +26,20 @@ pub const CONTROL_DATA_SIZE: usize = 20;
 impl ControlData {
     pub fn verify(&self) -> bool {
         let raw_bytes: [u8; CONTROL_DATA_SIZE] = unsafe { transmute(*self) };
-        (!(raw_bytes[4 .. CONTROL_DATA_SIZE - 4].iter()
-            .fold(0u32, |sum, val| sum.wrapping_add(*val as u32)))
-                == self.checksum) && (raw_bytes[.. 4] == *"SYNC".as_bytes())
+        if raw_bytes[.. 4] != *"SYNC".as_bytes() {
+            println!("[FFSim] ControlData: bad header! expected [53, 59, 4e, 43], got [{:x}, {:x}, {:x}, {:x}]",
+                     raw_bytes[0], raw_bytes[1], raw_bytes[2], raw_bytes[3]);
+            return false;
+        }
+
+        let expected: u32 = !(raw_bytes[4 .. CONTROL_DATA_SIZE - 4].iter()
+            .fold(0u32, |sum, val| sum.wrapping_add(*val as u32)));
+        if expected != self.checksum {
+            println!("[FFSim] ControlData: bad checksum! expected {}, got {}",
+                     expected, self.checksum);
+            return false;
+        }
+
+        return true;
     }
 }
